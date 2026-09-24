a569c45dedfe6ca4c59293c45e03db6881549b05
NanamiKyoka <NanamiKyoka@outlook.com>
2026-09-25 02:05:06 +0800

    refactor(tools): drop reserved categories and unregistered tool resources

--- app/src/main/java/com/nanami/koishi/core/model/ToolCategory.kt
+++ app/src/main/java/com/nanami/koishi/core/model/ToolCategory.kt
@@ -5,14 +5,6 @@ import com.nanami.koishi.R
 
 enum class ToolCategory(@StringRes val titleRes: Int) {
     ALL(R.string.category_all),
-    COMMON(R.string.category_common),
-    CALC_CONVERT(R.string.category_calc_convert),
-    DEV_CODE(R.string.category_dev_code),
-    HARDWARE(R.string.category_hardware),
-    IMAGE_APPS(R.string.category_text_image);
-
-    companion object {
-        @Deprecated("Renamed to IMAGE_APPS", ReplaceWith("IMAGE_APPS"))
-        val TEXT_IMAGE = IMAGE_APPS
-    }
+    LIFE(R.string.category_life),
+    IMAGE_APPS(R.string.category_image_apps);
 }
